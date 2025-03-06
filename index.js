3b628a0c66a306c597d674e7a8c664fa83b320ce
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-03-06 17:12:45 -0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+let userInfo = {
+  username: "user1",
+  password: "password123"
 };
+
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  while (attemptCount < 3) {
+    testPassword();
+  }
+  function testPassword() {
+    const passwordAttempt = prompt("Please enter your password");
+
+    if (passwordAttempt === userInfo.password) {
+      console.log("Login successful");
+      exit;
+    } else {
+      attemptCount++;
+      console.log(`Login failed. You have attempted ${attemptCount} login(s). You may try ${3 - attemptCount} more time(s) before your account is locked.`);
+    }
+    if (attemptCount >= 3) {
+      console.log("Account locked due to too many failed login attempts.");
+      alert("Account locked due to too many failed login attempts.");
+    }
+  }
+}
+
+createLoginTracker(userInfo);
